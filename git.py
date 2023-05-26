cf6d2c3620a3624e9224395afdcb9c67b9151bfc
Eugene <jeka.mikhailovv@gmail.com>
2023-05-26 16:14:23 +0300

    commit

--- git.py
+++ git.py
@@ -14,8 +14,9 @@ for i in range(1):
     for i in range(randint(0,5)):
         n2 = randint(numb-4,numb+4)
         v = str(n2) + " days ago"
-        with open("a.txt", 'a') as file:
-            file.write("a")
-        os.system('git add .')
-        os.system('git commit --date="' + v +'" -m "commit"')
-        os.system('git push origin main')
+        for i in range(randint(0,2)):
+            with open("a.txt", 'a') as file:
+                file.write("a")
+            os.system('git add .')
+            os.system('git commit --date="' + v +'" -m "commit"')
+            os.system('git push origin main')
