8f9c90dbf2a76573ee546d0637e503bca0d2530a
Eugene <jeka.mikhailovv@gmail.com>
2023-06-12 16:05:42 +0300

    commit

--- git.py
+++ git.py
@@ -9,7 +9,7 @@ for i in range(20):
         file.write("a")
     os.system('git add .')
     os.system('git commit --date="' + d +'" -m "commit"')
-    os.system('git push origin master')
+    os.system('git push origin main')
     for i in range(randint(0,2)):
         with open("a.txt", 'a') as file:
             file.write("a")
