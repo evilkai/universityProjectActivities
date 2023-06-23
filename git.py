40952c2f28bb5708eefee3ff5aea0f178c749170
Eugene <jeka.mikhailovv@gmail.com>
2023-06-23 16:02:55 +0300

    commit

---
+++ git.py
@@ -0,0 +1,18 @@
+import os
+from random import randint
+
+
+for i in range(1):
+    numb = randint(243, 244)
+    d =str(numb) + " days ago"
+    with open("a.txt", 'a') as file:
+        file.write("a")
+    os.system('git add .')
+    os.system('git commit --date="' + d +'" -m "commit"')
+    os.system('git push origin master')
+    for i in range(randint(0,2)):
+        with open("a.txt", 'a') as file:
+            file.write("a")
+        os.system('git add .')
+        os.system('git commit --date="' + d +'" -m "commit"')
+        os.system('git push origin master')
